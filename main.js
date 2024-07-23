4199fc7c9ab1fb9dd73631b776f1f23654e38e4f
Ignatius Ratemo <118337246+ignatiusratemo26@users.noreply.github.com>
2024-07-23 17:42:45 +0300

    Added promise chaining on main.js

--- main.js
+++ main.js
@@ -19,4 +19,31 @@ let order = (time, work) => {
   });
 };
 
-order(2000, () => console.log(`${stock.Fruits[0]}`));
+order(2000, () => console.log(`${stock.Fruits[0]}`))
+// promise chaining using .then() do not apply commas after then()
+// there are two arrow fuctions since order() accepts time and work()
+  .then(() => {
+    return order(0000, () => console.log("Production has started."));
+  })
+
+  .then(() => {
+    return order(2000, () => console.log("The fruit was chopped."));
+  })
+  .then(() => {
+    return order(1000, () =>
+      console.log(`${stock.liquid[0]} and ${stock.liquid[1]} was selected.`)
+    );
+  })
+  .then(() => {
+    return order(1000, () => console.log("start the machine."));
+  })
+  .then(() => {
+    return order(2000, () =>
+      console.log(`Icecream placed on ${stock.holder[0]} `)
+    );
+  })
+  .then(() => {
+    return order(2000, () =>
+      console.log(`${stock.toppings[0]} placed on icecream. `)
+    );
+  });
