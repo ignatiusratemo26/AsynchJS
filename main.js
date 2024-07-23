4d506869997b795cb7ea7d7fe7ba663cbe225333
Ignatius Ratemo <118337246+ignatiusratemo26@users.noreply.github.com>
2024-07-23 17:19:17 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+let stock = {
+  Fruits: ["strawberry", "mango", "vanilla", "grape"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"]
+};
+
+let is_shop_open = true;
+
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log("our shop is closed. "));
+    }
+  });
+};
+
+order(2000, () => console.log(`${stock.Fruits[0]}`));
